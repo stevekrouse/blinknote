64243f3f76275f73d23eaf322c44bbac621b370d
Steve Krouse <skrouse@seas.upenn.edu>
2018-11-23 12:31:35 +0000

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # blink-note
+
 new tab chrome extension - fastest way to take a note that'll stick around
+
+## Todos
+
+* make sure changes are saved before page is closed
+* show how many charcters are left
+* checkbox when synced
+* find a way to update the page when the data is changed in another tab without screwing things up (there's an onChanged event)
+* markdown styling stackedit style (augmented but still just markdown)
+  * start with just titles
+  * images
+  * lists
+  * links
+  * HTML
+* favicons / icons
+* publish to chrome store
+* broadcast: put on my website / tweet about it / put on HN / put on PH
+
+### Yak-shaving
+
+* fix variable names
+* extract debounce / throttle as own function
---
+++ index.js
@@ -0,0 +1,32 @@
+let editor = document.querySelector('#c');
+let error = document.querySelector("#error");
+let splitKeys = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+let timeout; 
+chrome.storage.sync.get(null, items => {
+  // Rejoin text that we split up below
+  let text = splitKeys.map(i => items['text' + i]).filter(t => t).join()
+  editor.innerHTML = text;
+})
+editor.addEventListener('keyup', (e) => { 
+  clearTimeout(timeout);
+  timeout = setTimeout(() => { 
+    let text = editor.innerHTML; 
+    setText(text)
+  }, 500); 
+  
+  function setText(text) {
+    // Split up text into multiple keys because there's a 8000 char limit on keys
+    // https://developer.chrome.com/apps/storage#type-StorageArea
+    var texts = {}
+    splitKeys.forEach(i => {
+      texts['text' + i] = text.substring(i * 8000, (i + 1) *8000);
+    });
+    error.style.display = text.length > 100000 ? 'block' : 'none';
+    chrome.storage.sync.set(texts, e => {
+      if (chrome.runtime.lastError) {
+        alert("Check console for error")
+        console.log(chrome.runtime.lastError)
+      }
+    }); 
+  }
+});
