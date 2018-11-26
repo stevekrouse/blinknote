b779150b48967393bdfaad04321403d659679ceb
Dan Kantor <dan.kantor@gmail.com>
2018-11-26 10:53:44 -0500

    added onChange listener and status indicator

--- index.js
+++ index.js
@@ -1,34 +1,83 @@
-let editor = document.querySelector('#c');
-let error = document.querySelector("#error");
 let splitKeys = [0,1,2,3,4,5,6,7,8,9,10,11,12];
+let editor;
+let error;
 let timeout; 
-chrome.storage.sync.get(null, items => {
-  // Rejoin text that we split up below
-  let text = splitKeys.map(i => items['text' + i]).filter(t => t).join()
-  editor.innerHTML = text;
-})
-editor.addEventListener('keyup', (e) => { 
-  // change the font size, animated, of all lines beginning with # 
-  ([].slice.call(editor.querySelectorAll('*')).map(e => [e, e.innerText]).map(([e, text]) => [e, text.match(/^(#+)\s.*$/)]).filter(([e, m]) => m).map(([e, m]) => [e, m[1]]).forEach(([e,m]) => {e.style.transition = "font-size 0.5s cubic-bezier(0, 1.03, 1, 1) 0s"; e.style.fontSize = (30 - (3*m.length)) || 3}))
-  clearTimeout(timeout);
-  timeout = setTimeout(() => { 
-    let text = editor.innerHTML; 
-    setText(text)
-  }, 500); 
-  
-  function setText(text) {
-    // Split up text into multiple keys because there's a 8000 char limit on keys
-    // https://developer.chrome.com/apps/storage#type-StorageArea
-    var texts = {}
-    splitKeys.forEach(i => {
-      texts['text' + i] = text.substring(i * 8000, (i + 1) *8000);
-    });
-    error.style.display = text.length > 100000 ? 'block' : 'none';
-    chrome.storage.sync.set(texts, () => {
-      if (chrome.runtime.lastError) {
-        alert("Check console for error")
-        console.log(chrome.runtime.lastError)
+let status;
+
+const init = () => {
+  document.title = 'BlinkNote';
+  editor = document.querySelector('#c');
+  error = document.querySelector("#error");
+  status = document.querySelector("#status");
+  chrome.storage.sync.get(null, items => {
+    displayText(items);
+  });
+  editor.addEventListener('keyup', (e) => { 
+    setTitle('Saving'); 
+    clearTimeout(timeout);
+    timeout = setTimeout(() => { 
+      let text = editor.innerHTML;
+      setText(text)
+    }, 500);
+  });
+  chrome.storage.onChanged.addListener((changes, namespace) => {
+    if (namespace === 'sync') {
+      let items = {};
+      const keys = Object.keys(changes);
+      let editorText = editor.innerHTML;
+      keys.forEach(key => {
+        let newValue = changes[key].newValue;
+        if (newValue !== editorText) {
+          items[key] = newValue;
+        }
+      });
+      if (Object.keys(items).length > 0) {
+        displayText(items);
+        setTitle('Updated');
       }
-    }); 
-  }
+    }
+  });
+}
+
+const setText = (text) => {
+  // Split up text into multiple keys because there's a 8000 char limit on keys
+  // https://developer.chrome.com/apps/storage#type-StorageArea
+  let texts = {};
+  splitKeys.forEach(i => {
+    texts['text' + i] = text.substring(i * 8000, (i + 1) *8000);
+  });
+  error.style.display = text.length > 100000 ? 'block' : 'none';
+  chrome.storage.sync.set(texts, e => {
+    if (chrome.runtime.lastError) {
+      setTitle('Error');
+      console.log(chrome.runtime.lastError)
+    } else {
+      setTitle('Saved');
+    }
+  }); 
+};
+
+const displayText = (items) => {
+  // Rejoin text that we split up below
+  let text = splitKeys.map(i => items['text' + i]).filter(t => t).join();
+  editor.innerHTML = text;
+}
+
+const setTitle = (title) => {
+  document.title = `${title} - BlinkNote`;
+}
+
+window.addEventListener('load', (e) => {
+  init();
 });
+
+
+// TODOS
+//
+// extract debounce / throttle as own function
+// show how many characters are left
+// checkbox when synced
+// better favicons / icons
+// publish to chrome store
+// when changes come from another tab, cursor gets reset to beginning of div
+// broadcast: put on my website / tweet about it / put on HN / put on PH
